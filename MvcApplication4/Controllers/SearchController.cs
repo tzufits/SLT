ffa695cf055a7487751275f0057ed67bbfbf0462
Neta <netashilo@walla.com>
2012-12-16 13:47:26 +0200

    simple screen

---
+++ MvcApplication4/Controllers/SearchController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using MvcApplication4.Models;
+using System.IO;
+
+namespace MvcApplication4.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult LogOn(SearchModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model.Day == "sun")
+                {
+                    FormsAuthentication.SetAuthCookie(model.Day, model.Sex);
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("","");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        public Boolean isExists(LogOnModel model)
+        {
+            if (!System.IO.File.Exists(@"C:\Users\oener\Documents\trans.txt"))
+                return false;
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\oener\Documents\trans.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+                if (lines[i] == model.UserName && lines[i + 1] == model.Password)
+                    return true;
+                else
+                    i++;
+            return false;
+
+        }
+
+    }
+}
---
+++ MvcApplication4/Models/SearchModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcApplication4.Models
+{
+    public class SearchModel
+    {
+        [Required]
+        [Display(Name = "יום")]
+        public string Day { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "עיר")]
+        public string City { get; set; }
+
+        [Display(Name = "מין")]
+        public bool Sex { get; set; }
+    }
+}
