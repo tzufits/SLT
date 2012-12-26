ffd73f7a750211d50e25b49bd7acd9b6b9a3d336
natali <natalimo@post.jce.ac.il>
2012-12-26 13:01:07 +0200

    GREEN

--- SLT.Tests/UnitTest1.cs
+++ SLT.Tests/UnitTest1.cs
@@ -13,9 +13,10 @@ namespace SLT.Tests
             string[] array1 = { "123456", "moran", "levi", "Tel Aviv", "13:00" };
             string[] items = { null, null, null, "Tel Aviv", "13:00" };//true
             string[] items2 = { null, "yosi", "levi", "Tel Aviv", null };//false
+            string[] items3 = { null, null, "levi", "Tel Aviv", null };//false
 
             Controllers.SearchController a = new Controllers.SearchController();
-            Assert.AreEqual(true, a.isExists2(items, array1));//TRUE
+            Assert.AreEqual(true, a.isExists2(items3, array1));//TRUE
         }
     }
 }
