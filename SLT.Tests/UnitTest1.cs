1fdfa5ddedf1b741c79040f0a28a2cf517e16637
natali <natalimo@post.jce.ac.il>
2012-12-26 12:58:07 +0200

    RED

--- SLT.Tests/UnitTest1.cs
+++ SLT.Tests/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace SLT.Tests
             string[] items2 = { null, "yosi", "levi", "Tel Aviv", null };//false
 
             Controllers.SearchController a = new Controllers.SearchController();
-            Assert.AreEqual(true, a.isExists2(items2, array1));
+            Assert.AreEqual(true, a.isExists2(items2, array1));//false
         }
     }
 }
